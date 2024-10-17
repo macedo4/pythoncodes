819e077501b5135f0a13d86a017b2d67566f5794
macedobr <quatro.srmace666@gmail.com>
2024-10-17 14:17:02 -0400

    Aula 07 Python

---
+++ aulas/aula07operadores_aritmeticos.py
@@ -0,0 +1,54 @@
+#operadores aritmeticos
+# + soma
+# * multiplicação
+#/ divisão
+#/ divisão
+# ** potencia
+# % resto da divisão
+#// divisão inteira(divisão inteira nada de float
+
+#ordem de precedencia
+'''
+1) ()
+2) ** potencia
+3) *, /, //, %
+4) +, -
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

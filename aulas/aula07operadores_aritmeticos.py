8af699ff77f41e1ba9c3142a03976ff0d0035741
macedobr <quatro.srmace666@gmail.com>
2024-10-17 14:41:35 -0400

    aula07

--- aulas/aula07operadores_aritmeticos.py
+++ aulas/aula07operadores_aritmeticos.py
@@ -14,9 +14,22 @@
 3) *, /, //, %
 4) +, -
 
-
+posição de palavras
+{:=^20} centralizado
 
 '''
+print("pratica")
+recebevalor = int(input("Digite um valor: "))
+recebevlor2 = int(input("Digite outro valor: "))
+soma = recebevalor + recebevlor2
+multi = recebevalor * recebevlor2
+divi = recebevalor / recebevlor2
+potencia = recebevalor ** recebevlor2
+resto = recebevalor % recebevlor2
+#\n quebra a linha
+#,end='" não quebra a linha
+print(f'Informações: \n produto: {multi} - soma: {soma} - divi: {divi:.2f} - potencia: {potencia} - resto: {resto}')
+
 
 
 
